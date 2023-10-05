0f8dabff7af5a57f898c78bc8c481d26af024f64
Diego Ochoa <one.diego@gmail.com>
2023-10-05 20:59:01 -0300

    Se agrego nuevo script de la base de datos

--- config/instituto.sql
+++ config/instituto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-10-2023 a las 22:51:05
+-- Tiempo de generación: 06-10-2023 a las 01:57:20
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
